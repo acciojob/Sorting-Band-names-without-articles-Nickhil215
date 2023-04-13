fe4ce4b8b4063a5c87eefcf8ea2ca6655ebb13a8
Nikhil Vanamala <105160327+Nickhil215@users.noreply.github.com>
2023-04-13 15:36:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-//your code here
-
+let arr=document.getElementById("band");
+// console.log(arr);
+let res=arr.getElementsByTagName("li")
+let result=[];
+for(let i=0;i<res.length;i++){
+    result.push(res[i].innerText);
+}
+result.sort()
+console.log(result);
